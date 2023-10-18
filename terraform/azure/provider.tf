93e44c185aa9e9be04b522432a2769273ddcf6ea
wolffbruno <wolffbruno72@gmail.com>
2023-10-18 20:58:02 -0300

    fix(provider.tf): shorten storage_account_name to meet Azure's name length limit

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "terraform"
-    storage_account_name = "terraformstatebrunowolff"
+    storage_account_name = "terraformstatebrunowol"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
