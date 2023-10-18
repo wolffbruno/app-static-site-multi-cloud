d686a130f669b6fd3649f6d440a3d6f653e071e5
wolffbruno <wolffbruno72@gmail.com>
2023-10-18 20:34:27 -0300

    feat(terraform/azure/provider.tf): add backend configuration for AzureRM to store Terraform state remotely fix(terraform/azure/provider.tf): add newline at end of file to comply with POSIX standards

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = ">= 3.76.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformstatebrunowolff"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
