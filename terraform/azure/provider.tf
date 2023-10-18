89e9ec3e80374cfa2dd852473bd4588844b6f661
wolffbruno <wolffbruno72@gmail.com>
2023-10-18 20:54:03 -0300

    feat(pipeline-azure.yaml): add environment variables for Azure authentication to improve security refactor(provider.tf): remove hardcoded subscription_id and tenant_id to use environment variables for better security and flexibility

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -11,12 +11,9 @@ terraform {
     storage_account_name = "terraformstatebrunowolff"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "71c23e9c-8230-4fe3-899a-a6c8abb23074"
-  tenant_id       = "11dbbfe2-89b8-4549-be10-cec364e59551"
 }
