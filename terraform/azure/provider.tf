439bd6859fa5fe96bca5505cf81670a2e2e912eb
wolffbruno <wolffbruno72@gmail.com>
2023-10-18 21:10:46 -0300

    refactor(provider.tf): remove required_providers block to simplify code feat(provider.tf): add alias 'cloud' to azurerm provider for better identification and usage in multi-provider scenarios

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.76.0"
-    }
-  }
-
   backend "azurerm" {
     resource_group_name  = "terraform"
     storage_account_name = "terraformstatebrunowol"
@@ -16,4 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
+  alias = "cloud"
 }
